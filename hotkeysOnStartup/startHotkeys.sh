7478ade13f8d6fdc71bab350240a9fb9645067a4
SonkeWohler <sonke.a.wohler@gmail.com>
2020-12-05 15:19:14 +0200

    added a bash to record the preferred order of scripts

---
+++ hotkeysOnStartup/startHotkeys.sh
@@ -0,0 +1,8 @@
+### --- this is the order in which the scripts should be started
+### --- not following this order can lead to interference between the scope of hotkeys
+### --- e.g. when switching windows the hotkeys might conflict for a moment
+### --- this has to do with which script takes priority, and the easiest way to manage is to start them in the right order
+
+### --- Also note: if the scripts are already running the confirmation windows you get might create issues by reversing the order
+start chrome.ahk
+start bash.ahk
