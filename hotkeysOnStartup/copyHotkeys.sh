ad4b306fbd26de1c31a0eb64450de4fb3a2e8062
Sonke Wohler <sonke.a.wohler@gmail.com>
2020-12-22 19:22:39 +0200

    fixed issues with deploying hotkey scripts

--- hotkeysOnStartup/copyHotkeys.sh
+++ hotkeysOnStartup/copyHotkeys.sh
@@ -1,3 +1,5 @@
 ### this script copies the autohotkey scripts into the location where the system will look for them on startup.
 ### It is essentially deploying the scripts rto production. See the readme why you should be carefulw ith this.
-cp -vr ./scripts ~/hotkeysOnStartup/
+cd scripts/
+cp -vr ./ ~/hotkeysOnStartup/
+cd -
--- hotkeysOnStartup/scripts/startHotkeys.sh
+++ hotkeysOnStartup/scripts/startHotkeys.sh
@@ -2,6 +2,7 @@
 ### --- not following this order can lead to interference between the scope of hotkeys
 ### --- e.g. when switching windows the hotkeys might conflict for a moment
 ### --- this has to do with which script takes priority, and the easiest way to manage is to start them in the right order
+cd ~/hotkeysOnStartup
 
 ### --- Also note: if the scripts are already running the confirmation windows you get might create issues by reversing the order
 start chrome.ahk
