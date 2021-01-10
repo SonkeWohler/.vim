8fb7540022e8e2bf1f1301fdb376285f61959d0b
SonkeWohler <sonke.a.wohler@gmail.com>
2021-01-11 00:56:03 +0200

    fixed issue with copying hotkey scripts to ~

--- hotkeysOnStartup/copyHotkeys.sh
+++ hotkeysOnStartup/copyHotkeys.sh
@@ -1,5 +1,6 @@
 ### this script copies the autohotkey scripts into the location where the system will look for them on startup.
 ### It is essentially deploying the scripts rto production. See the readme why you should be carefulw ith this.
 cd scripts/
-cp -vr ./ ~/hotkeysOnStartup/
+rm --force --recursive --verbose ~/hotkeysOnStartup/
+cp --recursive --verbose ./ ~/hotkeysOnStartup/
 cd -
