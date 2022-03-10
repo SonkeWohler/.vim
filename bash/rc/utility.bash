bbe8970cf0fb6c053070e7b284574ccb2e9410b4
sonke <sonke.a.wohler@gmail.com>
2022-03-10 14:01:49 +0200

    some install and suff

--- bash/rc/utility.bash
+++ bash/rc/utility.bash
@@ -133,4 +133,14 @@ dattach() {
   docker exec -i -t $1 /bin/bash
 }
 
+alias dcom='docker-compose'
+
 alias dockerQa='docker system prune -a'
+
+### --- package manager
+# I should (eventually) adapt this to work on any package manager and use some way to detect the correct one.  Not now though.
+
+#-- pacman
+
+# install but update before you do
+alias install='sudo pacman -Syu && sudo pacman -S '
