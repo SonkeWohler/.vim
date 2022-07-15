fc7582f1af9493b7d4ea2d9ed16cd67114cbae40
sonke <sonke.a.wohler@gmail.com>
2022-07-15 18:38:35 +0300

    feat(cmd): make use of ripgrep

--- neovim/lua/commands.lua
+++ neovim/lua/commands.lua
@@ -1,9 +1,11 @@
 -- ## commands
 
-cmd('PP', 'PackerSync', {desc = 'run PackerSync'})
-cmd('FF', 'Files', {desc = 'run fzf :Files'})
-cmd('LL', 'BLines', {desc = 'run fzf :BLines, the default is inside the current buffer'})
-cmd('LB', 'Lines', {desc = 'run fzf :Lines, not in Buffer'})
+cmd('PP', 'PackerSync', {desc = 'run PackerSync'} )
+cmd('FF', 'Files', {desc = 'run fzf :Files'} )
+cmd('LL', 'BLines', {desc = 'run fzf :BLines, the default is inside the current buffer'} )
+cmd('LB', 'Lines', {desc = 'run fzf :Lines, not in Buffer'} )
+cmd('RR', 'Rg', {desc = 'run fzf :Rg, in the working directory'} )
+cmd('RG', 'Rg', {desc = 'run fzf :Rg, in the working directory'} )
 cmd('Q', 'q!', {desc = 'force quit'})
 cmd('Qa', 'qa!', {desc = 'force quit all buffers'})
 
