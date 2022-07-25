b4d1c8f2347370c74b2900ba45743b8eeedda23a
sonke <sonke.a.wohler@gmail.com>
2022-07-25 22:21:19 +0300

    fix(ft:typescript: allow absoluate paths in imports

--- neovim/after/ftplugin/typescript.lua
+++ neovim/after/ftplugin/typescript.lua
@@ -1,5 +1,12 @@
 vim.opt_local.suffixesadd:prepend('.component.ts')
 vim.opt_local.suffixesadd:prepend('.component.html')
+vim.opt_local.path:prepend('../../')
+vim.opt_local.path:prepend('../../../')
+vim.opt_local.path:prepend('../../../../')
+vim.opt_local.path:prepend('../../../../../')
+vim.opt_local.path:prepend('../../../../../../')
+vim.opt_local.path:prepend('../../../../../../../')
+vim.opt_local.path:prepend('../../../../../../../../')
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
