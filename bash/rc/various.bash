d4c17928021ca27329d590897356304a642bef78
sonke <sonke.a.wohler@gmail.com>
2022-08-24 14:57:52 +0300

    feat(bash): better update and flatpak commands

--- bash/rc/various.bash
+++ bash/rc/various.bash
@@ -2,7 +2,11 @@
 ### --- package manager
 # I should (eventually) adapt this to work on any package manager and use some way to detect the correct one.  Not now though.
 
-#-- pacman
+#-- package managers and installation stuff
 
+# flatpak in three letters
+alias fpk='flatpak'
 # install but update before you do
-alias install='sudo pacman -Syu && sudo pacman -S '
+alias install='sudo pacman -Syu && yay -Syu && sudo pacman -S'
+# update the whole thing
+alias update-myself='sudo pacman -Syu && yay -Syu && sudo flatpak update'
