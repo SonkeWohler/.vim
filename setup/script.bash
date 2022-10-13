db087d8d1702cd7944147a23a98a694432321d29
sonke <sonke.w@knosc.com>
2022-10-13 10:11:57 +0300

    fix(setup): use .bash extention for scripts

--- setup/script.bash
+++ setup/script.bash
@@ -1,12 +1,12 @@
 cd ../config
 configs=$(pwd)
 
-sudo touch /bin/xswap
-echo '#!/bin/bash' | sudo tee /bin/xswap
-echo "xmodmap $configs/swap.xmodmap" | sudo tee -a /bin/xswap
-sudo chmod +x /bin/xswap
+sudo touch /bin/xswap.bash
+echo '#!/bin/bash' | sudo tee /bin/xswap.bash
+echo "xmodmap $configs/swap.xmodmap" | sudo tee -a /bin/xswap.bash
+sudo chmod +x /bin/xswap.bash
 
-sudo touch /bin/start_alacritty
-echo '#!/bin/bash' | sudo tee /bin/start_alacritty
-echo "alacritty --working-directory $configs/.. -e tmux"| sudo tee -a /bin/start_alacritty
-sudo chmod +x /bin/start_alacritty
+sudo touch /bin/start_alacritty.bash
+echo '#!/bin/bash' | sudo tee /bin/start_alacritty.bash
+echo "alacritty --working-directory $configs/.. -e tmux"| sudo tee -a /bin/start_alacritty.bash
+sudo chmod +x /bin/start_alacritty.bash
