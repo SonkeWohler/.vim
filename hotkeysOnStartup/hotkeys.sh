4ad60f523ca8871da30b312d0051a7361513e1a9
SonkeWohler <sonke.a.wohler@gmail.com>
2022-10-16 11:58:57 +0300

    feat(hotkey): startup script and somoe removed keys

---
+++ hotkeysOnStartup/hotkeys.sh
@@ -0,0 +1,3 @@
+### put this in C:/User/<username>/AppData/Roaming/Microsoft/Windows/Start\ Menu/Programs/Start-up/
+### to call my autohotkey scripts, provided you already copied them into the location below.
+start ~/hotkeysOnStartup/startHotkeys.sh
