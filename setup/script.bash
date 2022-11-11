eeeb56d66a9a60e002883108f0677be15bbb6574
sonke <sonke.a.wohler@gmail.com>
2022-11-11 11:41:31 +0200

    fix(setup): xswap without .bash, more convenient

--- setup/script.bash
+++ setup/script.bash
@@ -1,10 +1,10 @@
 cd ../config
 configs=$(pwd)
 
-sudo touch /bin/xswap.bash
-echo '#!/bin/bash' | sudo tee /bin/xswap.bash
-echo "xmodmap $configs/swap.xmodmap" | sudo tee -a /bin/xswap.bash
-sudo chmod +x /bin/xswap.bash
+sudo touch /bin/xswap
+echo '#!/bin/bash' | sudo tee /bin/xswap
+echo "xmodmap $configs/swap.xmodmap" | sudo tee -a /bin/xswap
+sudo chmod +x /bin/xswap
 
 sudo touch /bin/start_alacritty.bash
 echo '#!/bin/bash' | sudo tee /bin/start_alacritty.bash
