e2106722aae7030798566c0deddcd4ea333f63d8
sonke <sonke.w@knosc.com>
2022-12-31 01:21:56 +0200

    new(plug): testing out telescope

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -162,6 +162,28 @@ require('packer').startup(function()
   -- fuzzy finding with fzf
   -- NOTE: requires fzf and ripgrep installed on the system.
   use 'junegunn/fzf.vim'
+  -- telescope and related
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} },
+    after = 'telescope-fzf-native.nvim',
+    config = function()
+      require('telescope').setup {
+        extensions = {
+          fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+          }
+        }
+      }
+      require('telescope').load_extension('fzf')
+    end
+  }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
+
   ------ motions ------
   -- like tpope's surround but more maintained
   use {
