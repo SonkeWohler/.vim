e41bef72ffbcaf930d482805865e6e5c1668969d
sonke <sonke.a.wohler@gmail.com>
2023-02-15 10:04:40 +0200

    new(plug): firenvim from browser
    This allows me to use nvim from within my browser.  It doesn't fully
    work on Vivaldi - (<eyeroll>) with Jira/Confluence just not working -
    plus many input fields are just too small for this to be useful
    (e.g. reddit) - statusline etc takes up too much space.  But it is a
    cool idea and it is nice to be able to play with it when I am in the
    mood.
    
    https://chrome.google.com/webstore/detail/firenvim/egpjdkipkomnmjhjmdamaniclmdlobbo

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -543,6 +543,13 @@ end,
       end
     end
   }
+
+  -- use nvim inside browser
+  use {
+    'glacambre/firenvim',
+    run = function() vim.fn['firenvim#install'](0) end
+  }
+
 end)
 
 ----------------------------------------------
