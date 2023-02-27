e7fe16ba8e870c09e80914435d97cce566d21280
sonke <sonke.w@knosc.com>
2023-02-27 11:19:04 +0200

    feat(map): find files from window commands
    Sometimes I just find myself pressing <C-W> a lot and using the mappings
    I have set up with it.  Usually this is about arranging my buffers in
    some way that I want them to be so I will be able to focus in the next
    step.  Finding files is part of that workflow, so I might as well make
    it part of that family of mappings.
    
    I may make an effort to use the hydra more for this stuff, but for some
    reason it isn't really working for me on this front.  Either way, if it
    would work I would add it there as well.

--- neovim/lua/mappings.lua
+++ neovim/lua/mappings.lua
@@ -47,6 +47,8 @@ nmap('<c-w>>', ':lua require("smart-splits").resize_right(3)<CR>')
 nmap('<c-w>=', ':lua require("smart-splits").resize_up(3)<CR>')
 nmap('<c-w>-', ':lua require("smart-splits").resize_down(3)<CR>')
 nmap('<c-w>+', '<c-W>=')
+-- fuzzy find files, since I do this all the time
+nmap('<c-w>f', ':lua require("telescope.builtin").find_files()<CR>')
 -- more window controls are defined in the hydra-<C-W>
 
 ---- intra-buffer navigation ----
