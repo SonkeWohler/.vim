4644211ed360a898c15148a3584e20b0848e77a2
sonke <sonke.a.wohler@gmail.com>
2023-03-13 13:20:53 +0200

    feat(hydra): clean up unused
    I can't seem to make use of hydra as much as I thought I would.  Perhaps
    I just need to walk before I run with it, or perhaps it's not for me.
    
    The issue that makes me remove it is that it does take up keymaps, which
    sometimes interferes.  All the recent times I have edited the hydras has
    been about these conflicts, but I have not used a hydra in a long time.
    So it is time to remove those.
    
    The only exception may be the window management hydra, which has not
    been interfering with anything so far.  I think I even did use it a
    little while ago to adjust some splits.  I don't use it often and the
    more complex parts of it I don't remember, but it still has a positive
    impact in the bottom line.
    
    All other hydras, however, have been more annoying than anything else
    over the past few months.
    
    This is kind of sad, because I think they have a lot of potential.  They
    sound like such a good idea.  They just don't work for me right now, at
    all.

--- neovim/lua/hydras.lua
+++ neovim/lua/hydras.lua
@@ -1,84 +1,19 @@
 local Hydra = require('hydra')
 
------ LSP -----
-Hydra({
-  name = 'hydra-lsp',
-  mode = 'n',
-  body = ',,',
-  config = {
-    foreign_keys = nil,
-    invoke_on_body = true,
-  },
-  heads = {
-    {'n', ':lua vim.diagnostic.goto_next()<cr>', { desc = 'go to next diagnostic' }},
-    {'N', ':lua vim.diagnostic.goto_prev()<cr>', { desc = 'go to last diagnostic' }},
-    {'d', '<cmd>Telescope lsp_definitions<CR>', {
-      exit = true,
-      desc = 'go to definition',
-    }},
-    {'D', '<cmd>Telescope lsp_references<CR>', {
-      exit = true,
-      desc = 'view references' ,
-    }},
-    {'t', '<cmd>Telescope lsp_type_definitions<CR>', {
-      exit = true,
-      desc = 'go to type definition',
-    }},
-    {'T', '<cmd>Telescope diagnostics<CR>', {
-      exit = true,
-      desc = 'diagnostics',
-    }},
-    {'I', '<cmd>Telescope lsp_implementations<CR>', { exit = true, desc = 'view implementation' }},
-    {'k', ':lua vim.lsp.buf.hover()<cr>', { exit = true, desc = 'hover info' }},
-    {'K', ':lua vim.lsp.buf.signature_help()<cr>', { exit = true, desc = 'signature info' }},
-    {'l', ':lua vim.lsp.buf.code_action()<cr>', {
-      exit = true,
-      desc = 'code action',
-    }},
-    {'r', ':lua vim.lsp.buf.rename()<cr>', { exit = true, desc = 'rename under the cursor' }},
-    --nmap('gw', ':lua vim.lsp.buf.document_symbol()<cr>')
-    --{'w', ':lua vim.lsp.buf.workspace_symbol()<cr>', { desc = '' }},
-    {'<c-e>', '<c-e>'},
-    {'<c-y>', '<c-y>'},
-    {'<Esc>', nil, { exit = true, desc = 'quit' }},
-    {';', nil, { exit = true, desc = 'quit' }},
-    {'q', nil, { exit = true, desc = 'quit' }},
-    {'i', '"_ci', { exit = true }},
-    {'t', '"_ct', { exit = true }},
-    {'T', '"_cT', { exit = true }},
-    {'a', '"_ca', { exit = true }},
-    {'f', '"_cf', { exit = true }},
-    {'F', '"_cF', { exit = true }},
-    {'c', '"_cc', { exit = true }},
-    {'w', '"_cw', { exit = true }},
-    {'W', '"_cW', { exit = true }},
-    {'e', '"_ce', { exit = true }},
-    {'E', '"_cE', { exit = true }},
-    {'1', '"_c1', { exit = true }},
-    {'2', '"_c2', { exit = true }},
-    {'3', '"_c3', { exit = true }},
-    {'4', '"_c4', { exit = true }},
-    {'5', '"_c5', { exit = true }},
-    {'6', '"_c6', { exit = true }},
-    {'7', '"_c7', { exit = true }},
-    {'8', '"_c8', { exit = true }},
-    {'9', '"_c9', { exit = true }},
-  },
-})
-
--- -- mappings to complement the hydra
--- -- nmap('gd', ':lua vim.lsp.buf.definition()<cr>')
--- nmap('gd', '<cmd>Telescope lsp_definitions<CR>')
--- nmap('gD', ':lua vim.lsp.buf.type_definition()<cr>')
--- nmap('gi', ':lua vim.lsp.buf.implementation()<cr>')
--- --nmap('gw', ':lua vim.lsp.buf.document_symbol()<cr>')
--- nmap('gw', ':lua vim.lsp.buf.workspace_symbol()<cr>')
--- -- nmap('gr', ':lua vim.lsp.buf.references()<cr>')
--- nmap('gr', '<cmd>Telescope lsp_references<CR>')
--- nmap('K', ':lua vim.lsp.buf.hover()<cr>')
--- -- nmap('<c-k>', ':lua vim.lsp.buf.signature_help()<cr>')
--- -- nmap('L', ':lua vim.lsp.buf.code_action()<cr>')
--- -- nmap('R', ':lua vim.lsp.buf.rename()<cr>')
+-- hydras sounded more cool than they have been for me.  I am not sure why I
+-- don't seem to be using them, but I suspect it is mostly because when
+-- something does have a long key-sequence I just never use it.  The fact that
+-- hydra requires me to only type out that sequence once doesn't help me
+-- remember it.  Perhaps I'll figure it out one day.
+--
+-- An added problem is that I sometimes run across built in keymaps that I just
+-- never realised existed.  At time of writing the most recent one is comma (,),
+-- which looks to be the reverse of semi-colon (;).  I have googled for a
+-- reverse for (;), but couldn't find it, and now I just ran across this reading
+-- the treesitter-textobjects documentation.
+--
+-- So while hydras are vim on steroids, they seem to be a bit too much for me to
+-- handle right now.  I'll walk before I run.
 
 ----- Windows -----
 Hydra({
@@ -146,24 +81,3 @@ Hydra({
     -- TODO resize
   }
 })
-
---[[ ----- GitSigns ------
-Hydra({
-  name = 'hydra-git',
-  mode = 'n',
-  body = ';;',
-  config = {
-    foreign_keys = 'warn',
-    invoke_on_body = true,
-  },
-  heads = {
-    {'n', ':lua require("gitsigns").next_hunk()<CR>', { desc = 'next hunk' } },
-    {'N', ':lua require("gitsigns").prev_hunk()<CR>', { desc = 'previous hunk' } },
-    {'s', ':lua require("gitsigns").stage_hunk()<CR>', { desc = 'stage hunk' } },
-    {'u', ':lua require("gitsigns").undo_stage_hunk()<CR>', { desc = 'unstage hunk' } },
-    {'<c-e>', '<c-e>'},
-    {'<c-y>', '<c-y>'},
-    {'<Esc>', nil, { exit = true, desc = 'quit' }},
-    {'q', nil, { exit = true, desc = 'quit' }},
-  }
-}) ]]
