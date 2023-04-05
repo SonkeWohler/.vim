bfe271c4cbc063d8ef1ef007fad7d600d47a72a5
sonke <sonke.a.wohler@gmail.com>
2023-04-05 13:04:56 +0300

    feat(bash): clean and update docker/kubernetes
    There were a bunch of old commands for minikube and docker-compose left
    over from back when.  I don't use those anymore.
    
    But I do sometimes want to have a json prettyfied log for a deployment
    or job.  And I don't always want to have to do this inside k9s.

--- bash/rc/docker.bash
+++ bash/rc/docker.bash
@@ -1,32 +1,33 @@
 
-### --- docker
-# WIP #
-# all this is experimental
-# WIP #
-
-dattach() {
-  docker exec -i -t $1 /bin/bash
-}
-kattach() {
-  kubectl exec --stdin --tty $1 -- /bin/bash
-}
-# https://stackoverflow.com/questions/47389443/finding-the-name-of-a-new-pod-with-kubectl
-# attach to knosc kubernetes pod
-kktach() {
-  kattach $(ktl get pod -l app=$1 -o jsonpath="{.items[0].metadata.name}")
-}
-
-
 ### --- kubernetes
 
 alias kk='k9s'
 alias ktl='kubectl'
-alias mkl='minikube kubectl'
-alias mk='minikube'
-alias ktlc='kubectl config set-context'
 
-alias dcom='docker-compose'
-alias dcomup='docker-compose up -d --remove-orphans'
-alias dcomp='dcomup'
+klogpods() {
+    # makes it easier to see the start of stream when quickly scrolling through
+    echo '========================================='
+    echo '-----------------------------------------'
+    echo '========================================='
+    kubectl logs -f $1 -n $2 --context $3 | pino-pretty --colorize
+}
 
-alias dockerQa='docker system prune -a'
+klogp() {
+    klogpods $1 development docker-desktop
+}
+
+kloglabel() {
+    # makes it easier to see the start of stream when quickly scrolling through
+    echo '========================================='
+    echo '-----------------------------------------'
+    echo '========================================='
+    kubectl logs -f -l $1 -n $2 --context $3 | pino-pretty --colorize
+}
+
+
+klogd() {
+    kloglabel app=$1 development docker-desktop
+}
+klogj() {
+    kloglabel job-name=$1 development docker-desktop
+}
