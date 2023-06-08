1ae2c094ecc0fc44627dbc3b2b6ca24875b82e54
sonke <sonke.a.wohler@gmail.com>
2023-06-08 12:30:02 +0300

    feat(config): autostart
    This is run automatically on startup - at least on plasma.

--- setup/kde.bash
+++ setup/kde.bash
@@ -7,11 +7,15 @@ echo "you should restart your xsession after this to see whether everything work
 echo "any files that are replaced are copied to $file.old, so you should be able to restore things from a terminal session relatively easily"
 echo "------------------"
 
+# desktop background
 cd ../config
 sudo mkdir /usr/share/backgrounds
 sudo chmod -R +r /usr/share/backgrounds
 cp -v ./backgrounds/ddforest.jpeg /usr/share/backgrounds/
 
+# all them kde files
 ./copykde.sh
 cd -
 
+# autostart
+ln --symbolic --verbose $vimCD/config/autostart ~/.config/autostart
