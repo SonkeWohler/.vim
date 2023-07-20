0a1e1f28fc1fe22f8c49fed16eef1c4469d86f9e
sonke <sonke.a.wohler@gmail.com>
2023-07-20 10:52:58 +0300

    feat(ftplugin/md): less text only
    I have started to write a significant amount of code inside markdown
    code-fences.  And then these are more annoying than helpful.  I have
    generally started getting used to not using them anyway, so I might even
    remove them entirely.

--- neovim/after/ftplugin/markdown.lua
+++ neovim/after/ftplugin/markdown.lua
@@ -2,7 +2,7 @@ vim.opt.spell = true
 -- autoindent after formatting
 nmap('==', 'mhgqip=ip`h')
 -- break undo at (half-)sentences
-imap('.  ', '.  <c-g>u')
-imap(', ', ', <c-g>u')
-imap('! ', '! <c-g>u')
-imap('?  ', '?  <c-g>u')
+-- imap('.  ', '.  <c-g>u')
+-- imap(', ', ', <c-g>u')
+-- imap('! ', '! <c-g>u')
+-- imap('?  ', '?  <c-g>u')
