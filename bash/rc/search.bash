0e7edb6f3a66b38018f5f82ccff20ed554c5ec29
sonke <sonke.w@knosc.com>
2023-07-24 10:13:03 +0300

    feat(bash/search): cleanup and pagers
    I prefer using pagers where possible, so I added them here.  And while I
    was here I realized there were a lot of old functions and aliases that I
    have not used in a long time.  Some of them I probably never used.

---
+++ bash/rc/search.bash
@@ -0,0 +1,22 @@
+### --- ripgrep and related
+
+# configure ripgrep
+# since this doesn't have a default config file location?
+export RIPGREP_CONFIG_PATH="$vimCD/config/ripgreprc"
+
+# search with pager, I am experimenting with different options
+rgg() { rg --json $@ | delta ; }
+alias rgb='batgrep'
+
+### --- general paging - mostly bat
+
+# basically batman, but this way I get completion
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+export MANROFFOPT="-c"
+
+### --- find = fd
+
+# usually I prefer having a pager, because then scrolling up isn't ass spammed
+fdd() { fd $@ | delta ;}
+# include hidden, but not git, files only
+ff() { fdd --hidden --type f --exclude .git $@ | delta ;}
