20212c17b7e92e94617a2517a89904fea97dd1e5
sonke <sonke.a.wohler@gmail.com>
2023-08-10 13:48:19 +0300

    feat(bash/common): pager options
    When quitting my pager wipe the screen/do not dump the pager view.
    
    This may finally allow me to use git aliases instead of bash aliases for
    some of what I have in bash/rc/git.bash

--- bash/common.bash
+++ bash/common.bash
@@ -1,7 +1,14 @@
 ### stuff used throughout by bashrc files
+# I'll also throw options in here, since they affect all of bash
+# at least until I have too many options
 
 ### --- paging
 
 # I prefer full paging, because it creates less clutter in my display
 alias dlt='delta --paging=always'
-
+# this took me way too long to find and understand
+# https://superuser.com/questions/366930/how-do-i-get-the-git-pager-to-clean-up-screen-output-after-exit
+# but I prefer pagers to NOT dump their view in the terminal on quit
+# pagers are for paging, not for dumping!
+# works with delta as well
+export LESS=RS
