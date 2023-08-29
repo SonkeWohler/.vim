4a8be1b2669a396b3737737fae85c503191bbb1c
sonke <sonke.a.wohler@gmail.com>
2023-08-29 11:00:43 +0300

    ref(bash/settings): LESS is a setting

--- bash/common.bash
+++ bash/common.bash
@@ -6,9 +6,3 @@
 
 # I prefer full paging, because it creates less clutter in my display
 alias dlt='delta --paging=always'
-# this took me way too long to find and understand
-# https://superuser.com/questions/366930/how-do-i-get-the-git-pager-to-clean-up-screen-output-after-exit
-# but I prefer pagers to NOT dump their view in the terminal on quit
-# pagers are for paging, not for dumping!
-# works with delta as well
-export LESS=RS
--- bash/rc/settings.bash
+++ bash/rc/settings.bash
@@ -8,3 +8,9 @@ export EDITOR="nvim"
 # for sudoedit
 export SUDO_EDITOR="nvim"
 
+# this took me way too long to find and understand
+# https://superuser.com/questions/366930/how-do-i-get-the-git-pager-to-clean-up-screen-output-after-exit
+# but I prefer pagers to NOT dump their view in the terminal on quit
+# pagers are for paging, not for dumping!
+# works with delta as well
+export LESS=RS
