0b86b8cf98cfe2ce1f57cd0238e9fe26d9665f9d
sonke <sonke.a.wohler@gmail.com>
2023-11-06 20:17:27 +0200

    fix(bash/util): update nvim separately
    This command is kind of broken.  But since I get notifications on nvim
    updates every time I open it I don't need to worry about forgetting
    this.

--- bash/rc/utility.bash
+++ bash/rc/utility.bash
@@ -65,7 +65,7 @@ updateCargo() {
 # note that due to conflicts between npm and pacman I don't usually update npm
 # unless I have to
 # alias updateAll='sudo pacman -Syu && yay && updateNvim && cargo install-update -a && flatpak update && sudo npm --global update'
-alias updateAll='sudo pacman -Syu && yay && updateNvim && updateCargo && flatpak update'
+alias updateAll='sudo pacman -Syu && yay && updateCargo && flatpak update'
 # pull important repos, rebuild and list new commits so I know if there is
 # anything I need to do
 alias pullAll='cdw && gitp && make rebuild-dev && gitln && cdv && gitp'
