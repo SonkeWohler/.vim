4916ac28aa65de3db5559d6e29cebdacbd1e0e92
sonke <sonke.a.wohler@gmail.com>
2023-11-27 18:55:20 +0200

    feat!(setup/pacman): install jq
    ```
    sudo pacman -Syu && sudo pacman -S jq --noconfirm
    ```
    
    I can't quite believe I have never felt the need to add it here before.

--- setup/pacman.bash
+++ setup/pacman.bash
@@ -24,6 +24,7 @@ sudo pacman -S fzf --noconfirm
 sudo pacman -S ripgrep --noconfirm
 sudo pacman -S ripgrep-all --noconfirm
 sudo pacman -S repgrep --noconfirm
+sudo pacman -S jq --noconfirm
 # cli paging and formatting
 sudo pacman -S bat  --noconfirm
 sudo pacman -S bat-extras  --noconfirm
