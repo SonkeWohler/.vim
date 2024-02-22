3cc1ac29cf9ed2b1a63974926eabc5b1e98bac6c
sonke <sonke.a.wohler@gmail.com>
2024-02-22 14:28:55 +0200

    chore(plug/integration): obsidian upgraded some stuff and deprecated old stuff

--- neovim/lua/lazy_plugins/integrations.lua
+++ neovim/lua/lazy_plugins/integrations.lua
@@ -42,19 +42,28 @@ return {
         },
         dir = "~/nextcloud/sync/vault",
         mappings = {},
+        -- Whether to add the output of the node_id_func to new notes in autocompletion.
+        -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
+        -- https://github.com/epwalsh/obsidian.nvim/pull/406#issue-2140673715
+        wiki_link_func = function(opts)
+          if opts.id == nil then
+            return string.format("[[%s]]", opts.label)
+          elseif opts.label ~= opts.id then
+            return string.format("[[%s|%s]]", opts.id, opts.label)
+          else
+            return string.format("[[%s]]", opts.id)
+          end
+        end,
         completion = {
           -- If using nvim-cmp, otherwise set to false
           nvim_cmp = true,
           -- Trigger completion at 2 chars
           min_chars = 2,
-          -- Where to put new notes created from completion. Valid options are
-          --  * "current_dir" - put new notes in same directory as the current buffer.
-          --  * "notes_subdir" - put new notes in the default notes subdirectory.
-          new_notes_location = "current_dir",
-          -- Whether to add the output of the node_id_func to new notes in autocompletion.
-          -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
-          prepend_note_id = true
         },
+        -- Where to put new notes created from completion. Valid options are
+        --  * "current_dir" - put new notes in same directory as the current buffer.
+        --  * "notes_subdir" - put new notes in the default notes subdirectory.
+        new_notes_location = "current_dir",
         open_app_foreground = true,
         finder = "telescope.nvim",
         sort_by = "modified",
