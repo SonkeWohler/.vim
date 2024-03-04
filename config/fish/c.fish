a146a784ca39f2d2d98152929815bcea0586999d
sonke <sonke.a.wohler@gmail.com>
2024-03-04 20:20:24 +0200

    feat(config/fish): some more abbreviations
    But nothing major yet.  So I am still in between the two.

--- config/fish/c.fish
+++ config/fish/c.fish
@@ -3,9 +3,18 @@ if status is-interactive
     starship init fish | source
     fzf_configure_bindings --directory=\ct --git_status=\cs --git_log=\cg --history=\cr --variables=\cv --processes=\e\cp
 
-    # abbreviations
+    # --- functions
+    function delta-standard-pager --description 'delta with standard-options' ; delta --paging=always ; end
+    alias dlt delta-standard-pager
+    function rgg
+        rg --json $argv | delta-standard-pager
+    end
+
+    # --- abbreviations
     # usually just a copy of `abbr --show`
     abbr -a -- vi 'nvim -p'
     abbr -a -- vimu 'vim -u NONE'
     abbr -a -- nvimu 'nvim -u NONE'
+    abbr -a -- sedn 'sed -En'
+    abbr -a -- lsa 'ls -lAFh'
 end
