609eb132fe4efdd4ca2add66287e74f41efc4f13
sonke <sonke.a.wohler@gmail.com>
2024-03-04 11:27:55 +0200

    feat(config/*): small steps to switch to fish
    I really like the feel of this better, and it was trivial to set up.  By
    two problems are:
    * I have to convert my `gitdn` and similar git related aliases, and have
      not made up my mind on whether they should be 100% inside gitconfig
      which might require me to specify the shell for git aliases.
    * I haven't figured out how to properly synch the fish config.  Right
      now it complains that it can't install the plugins I added on the
      other machine, requiring me to delete my config and synch it again.

--- config/fish/c.fish
+++ config/fish/c.fish
@@ -2,4 +2,10 @@ if status is-interactive
     set fish_greeting "Asta e Pestisorul meu"
     starship init fish | source
     fzf_configure_bindings --directory=\ct --git_status=\cs --git_log=\cg --history=\cr --variables=\cv --processes=\e\cp
+
+    # abbreviations
+    # usually just a copy of `abbr --show`
+    abbr -a -- vi 'nvim -p'
+    abbr -a -- vimu 'vim -u NONE'
+    abbr -a -- nvimu 'nvim -u NONE'
 end
