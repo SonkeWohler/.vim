c6f64474c5606129ed85827efa33ec36ae0b8203
sonke <sonke.a.wohler@gmail.com>
2024-03-12 16:06:18 +0200

    feat(config/fish): abbr for kubernetes logs

--- config/fish/c.fish
+++ config/fish/c.fish
@@ -18,26 +18,54 @@ if status is-interactive
     # ----------------- #
     # --- functions --- #
     # ----------------- #
+    # I usually don't use short names here, to make it easier to reuse these in
+    # many places without loosing track of the different names.
+    # Convenient/short names are for abbreviations or aliases.
 
-    # --- standard options for various things
+    # --- standard options for various things - aka templates
     # --- standard does not mean I want it to be the default every time i use
     # --- it, so I can't just throw them into the config file
     # a pager with syntax and git highlighting
-    function delta-standard-pager --description 'delta with standard-options' ; delta --paging=always ; end
-    alias dlt delta-standard-pager
+    function delta-template --description 'delta with standard-options' ; delta --paging=always ; end
     function rgg --description 'rg with auto-page into my standard delta'
-        rg --json $argv | delta-standard-pager
+        rg --json $argv | delta-template
     end
+    # view kubernetes pod logs
+    function kubernetes-app-log-template --description 'view logs for an app on my local'
+        # makes it easier to see the start of stream when quickly scrolling through
+        echo '========================================='
+        echo '-----------------------------------------'
+        echo '========================================='
+        kubectl logs -n development --context docker-desktop -f -l app=$argv[1] | pino-pretty --colorize
+    end
+    function kubernetes-job-log-template --description 'view logs for a job on my local'
+        # makes it easier to see the start of stream when quickly scrolling through
+        echo '========================================='
+        echo '-----------------------------------------'
+        echo '========================================='
+        kubectl logs -n development --context docker-desktop -f -l job-name=$argv[1] | pino-pretty --colorize
+    end
+    function kubernetes-pod-log-template --description 'view logs for pod on my local'
+        # makes it easier to see the start of stream when quickly scrolling through
+        echo '========================================='
+        echo '-----------------------------------------'
+        echo '========================================='
+        kubectl logs -f -n development --context docker-desktop $argv[1]
+    end
+    # --- utillity
+    # change into directory after creating
     function make-and-change-into-directory --description 'change into the directory you created'
         mkdir -vp $argv[1]
         cd $argv[1]
     end
 
+
     # --------------------- #
     # --- abbreviations --- #
     # --------------------- #
-    # usually just a copy of `abbr --show`
-
+    # also aliases
+    alias dlt delta-template
+    # the rest is usually just a copy of `abbr --show`
     abbr -a -- cd- 'cd -'
     abbr -a -- cd. 'cd ..'
     abbr -a -- mkd 'mkdir -vp'
@@ -59,5 +87,9 @@ if status is-interactive
     abbr -a -- sshNXT 'ssh $nextcloud_server_ip'
     abbr -a -- viNXT 'cd $nextcloud_synch_PATH/vault && vi Index.md'
     abbr -a -- tre 'tree --gitfile="$dotfiles_PATH/config/gitignore" | grep -v "\->"'
+    abbr -a -- kk 'k9s'
+    abbr -a -- klogj 'kubernetes-job-log-template'
+    abbr -a -- klogd 'kubernetes-app-log-template'
+    abbr -a -- klogp 'kubernetes-pod-log-template'
 
 end
