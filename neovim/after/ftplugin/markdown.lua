b4c35c720a605b6cee175991cad6cd68fc60cd1d
sonke <sonke.a.wohler@gmail.com>
2025-04-15 16:07:25 +0300

    feat(nvim/markdown): hyphens are weird

--- neovim/after/ftplugin/markdown.lua
+++ neovim/after/ftplugin/markdown.lua
@@ -1,6 +1,9 @@
 vim.opt.spell = true
 -- autoindent after formatting
 nmap('==', 'mhgqip=ip`h')
+-- hyphens are weird in markdown, since they also denote list items
+-- imap(' - ', ' &ndash; ')
+nmap('--', 'a&ndash; ')
 -- break undo at (half-)sentences
 -- imap('.  ', '.  <c-g>u')
 -- imap(', ', ', <c-g>u')
