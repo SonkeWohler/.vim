832eac790f9721a07fc16b747f87db28c0f50cc5
sonke <sonke.a.wohler@gmail.com>
2026-02-24 20:44:34 +0200

    feat(setup/config): prompt symlink overwrite
    This is easier to stay on top of, to make sure all worked as intended,
    and allows overwrite, so you can repeat the thing.

--- setup/config.bash
+++ setup/config.bash
@@ -29,8 +29,8 @@ fi
 #-- git
 
 mv --verbose ~/.gitconfig ~/gitconfig.old
-ln --symbolic --verbose $vimCD/config/gitconfig ~/.gitconfig
-ln --symbolic --verbose $vimCD/config/gitignore ~/gitignore
+ln --symbolic --verbose $vimCD/config/gitconfig ~/.gitconfig --interactive
+ln --symbolic --verbose $vimCD/config/gitignore ~/gitignore --interactive
 
 #-- bash
 
@@ -44,24 +44,24 @@ echo "### --- source my version controlled bashrc" >> ~/.bashrc
 echo "" >> ~/.bashrc
 echo "source $vimCD/bash/bashrc" >> ~/.bashrc
 
-ln --symbolic --verbose $vimCD/bash/inputrc ~/.inputrc
+ln --symbolic --verbose $vimCD/bash/inputrc ~/.inputrc --interactive
 
 #-- tmux
 
 print "setting up tmux"
-ln --symbolic --verbose $vimCD/config/tmux.conf ~/.tmux.conf
+ln --symbolic --verbose $vimCD/config/tmux.conf ~/.tmux.conf --interactive
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins
 
 #-- alacritty
 
 print "setting up alacritty"
-ln --symbolic --verbose $vimCD/config/alacritty.toml ~/.config/
+ln --symbolic --verbose $vimCD/config/alacritty.toml ~/.config/ --interactive
 
 #-- starship
 
 print "setting up starship"
-ln --symbolic --verbose $vimCD/config/starship.toml ~/.config/starship.toml
+ln --symbolic --verbose $vimCD/config/starship.toml ~/.config/starship.toml --interactive
 
 #-- fish shell - experimental for now
 print "setting up fish and fisher"
@@ -87,46 +87,46 @@ fi
 # if test -e ~/.config/fish/themes; then
 #     rm -vdr ~/.config/fish/themes
 # fi
-ln --symbolic --verbose $vimCD/config/fish/c.fish ~/.config/fish/config.fish
-ln --symbolic --verbose $vimCD/config/fish/p ~/.config/fish/fish_plugins
-ln --symbolic --verbose $vimCD/config/fish/functions ~/.config/fish/functions
-ln --symbolic --verbose $vimCD/config/fish/conf.d ~/.config/fish/conf.d
-ln --symbolic --verbose $vimCD/config/fish/completions ~/.config/fish/completions
-ln --symbolic --verbose $vimCD/config/fish/fish_variables ~/.config/fish/fish_variables
-# ln --symbolic --verbose $vimCD/config/fish/themes ~/.config/fish/themes
+ln --symbolic --verbose $vimCD/config/fish/c.fish ~/.config/fish/config.fish --interactive
+ln --symbolic --verbose $vimCD/config/fish/p ~/.config/fish/fish_plugins --interactive
+ln --symbolic --verbose $vimCD/config/fish/functions ~/.config/fish/functions --interactive
+ln --symbolic --verbose $vimCD/config/fish/conf.d ~/.config/fish/conf.d --interactive
+ln --symbolic --verbose $vimCD/config/fish/completions ~/.config/fish/completions --interactive
+ln --symbolic --verbose $vimCD/config/fish/fish_variables ~/.config/fish/fish_variables --interactive
+# ln --symbolic --verbose $vimCD/config/fish/themes ~/.config/fish/themes --interactive
 
 #-- glow, used for neovim, but can be used independently
 
 print "setting up glow"
 mkdir --parents --verbose ~/.config/glow
-ln --symbolic --verbose $vimCD/config/glow.yml ~/.config/glow/glow.yml
+ln --symbolic --verbose $vimCD/config/glow.yml ~/.config/glow/glow.yml --interactive
 
 #-- k9s
 
 print "setting up k9s plugins"
 mkdir --parents --verbose ~/.config/k9s
-ln --symbolic --verbose $vimCD/config/k9s/p.yml ~/.config/k9s/plugins.yaml
-ln --symbolic --verbose $vimCD/config/k9s/c.yml ~/.config/k9s/config.yaml
+ln --symbolic --verbose $vimCD/config/k9s/p.yml ~/.config/k9s/plugins.yaml --interactive
+ln --symbolic --verbose $vimCD/config/k9s/c.yml ~/.config/k9s/config.yaml --interactive
 
 #-- newsboat, rss reader
 
 print "setting up newsboat - my rss reader"
 mkdir --parents --verbose ~/.newsboat
-ln --symbolic --verbose $vimCD/config/newsboat ~/.newsboat/config
+ln --symbolic --verbose $vimCD/config/newsboat ~/.newsboat/config --interactive
 
 #-- github cli
 
 print "setting up github cli config"
 print "github cli also requires you to go through 'gh auth login'"  # TODO
 mkdir --verbose --parents ~/.config/gh
-ln --symbolic --verbose $vimCD/config/githubcli.yml ~/.config/gh/config.yml
+ln --symbolic --verbose $vimCD/config/githubcli.yml ~/.config/gh/config.yml --interactive
 
 #-- neovim
 
 print "setting up neovim"
 
 mkdir --parents --verbose ~/.config/nvim
-ln --symbolic --verbose $vimCD/neovim/* ~/.config/nvim
+ln --symbolic --verbose $vimCD/neovim/* ~/.config/nvim --interactive
 
 # NOTE treesitter should be triggered by Lazy
 nvim -c "lua require('lazy').sync({wait = true})" -c 'autocmd User VeryLazy MasonUpdateAll'
@@ -137,12 +137,12 @@ nvim -c 'PylspInstall pylsp-mypy pyls-isort python-lsp-black pylsp-rope'
 
 #-- keyboard
 
-sudo mkdir /etc/keyd
-sudo ln --symbolic --verbose $vimCD/config/keyd.conf /etc/keyd/ && \
+sudo mkdir /etc/keyd --parents && \
+sudo ln --symbolic --verbose $vimCD/config/keyd.conf /etc/keyd/ --interactive && \
 sudo systemctl enable keyd --now && \
 sudo keyd reload
 
 #-- gitui
 
 mkdir --parents --verbose ~/.config/gitui
-ln --symbolic --verbose $vimCD/config/gitui/key_bindings.ron ~/.config/gitui/key_bindings.ron
+ln --symbolic --verbose $vimCD/config/gitui/key_bindings.ron ~/.config/gitui/key_bindings.ron --interactive
