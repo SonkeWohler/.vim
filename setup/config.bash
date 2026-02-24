076eeaddc4db8f0c2db1e09a9354a17c00c6e7ab
sonke <sonke.a.wohler@gmail.com>
2026-02-24 19:05:04 +0200

    fix(setup/config): nvim plugins
    I haven't used packer for a long time, nor firenvim

--- setup/config.bash
+++ setup/config.bash
@@ -128,15 +128,12 @@ print "setting up neovim"
 mkdir --parents --verbose ~/.config/nvim
 ln --symbolic --verbose $vimCD/neovim/* ~/.config/nvim
 
-nvim -c 'autocmd User PackerComplete quitall' -c 'PackerSync' && \
-nvim -c 'autocmd User MasonInstallAllComplete quitall' -c 'MasonInstallAll' -c 'Mason' && \
-nvim -c 'TSInstallSync all' -c 'quitall' && \
+# NOTE treesitter should be triggered by Lazy
+nvim -c "lua require('lazy').sync({wait = true})" -c 'autocmd User VeryLazy MasonUpdateAll'
 # https://github.com/williamboman/mason-lspconfig.nvim/blob/main/lua/mason-lspconfig/server_configurations/pylsp/README.md
 # if that moves, here is a commit that added this file
 # https://github.com/williamboman/mason-lspconfig.nvim/blob/1d459b6d19118b9944d5313e4439cb361d607366/lua/mason-lspconfig/server_configurations/pylsp/README.md
-nvim -c 'PylspInstall pylsp-mypy pyls-isort python-lsp-black pylsp-rope' && \
-# https://github.com/glacambre/firenvim/blob/master/TROUBLESHOOTING.md
-nvim -c 'call firenvim#install(0)'
+nvim -c 'PylspInstall pylsp-mypy pyls-isort python-lsp-black pylsp-rope'
 
 #-- keyboard
 
