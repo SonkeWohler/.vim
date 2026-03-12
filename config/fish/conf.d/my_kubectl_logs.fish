3a17d3dd22306741abafcdd38fc403f838787f64
sonke <sonke.a.wohler@gmail.com>
2026-03-12 18:46:32 +0200

    fixup! feat(config/fish): kindly can specify images and cluster

--- config/fish/conf.d/my_kubectl_logs.fish
+++ config/fish/conf.d/my_kubectl_logs.fish
@@ -115,6 +115,7 @@ end
 # but this might be nicer than docker desktop, which kept creating problems for me
 function kindly --description 'load docker images into the kindly cluster'
     argparse 'i/images=+' 'n/name' -- $argv
+    or return
     set -ql _flag_i[1]
     and set images $_flag_i
     or set images (docker images | grep knosc | awk '{print $1}')
